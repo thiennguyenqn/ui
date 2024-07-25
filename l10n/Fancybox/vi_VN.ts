a2a2179ee106286fa7fb48b07035a8b018d23713
yoi <44944761+thiennguyenqn@users.noreply.github.com>
2024-07-25 18:00:41 +0700

    Create vi_VN.ts
    Add Vietnamese language.

---
+++ l10n/Fancybox/vi_VN.ts
@@ -0,0 +1,20 @@
+import { en as PANZOOM_VI } from "../Panzoom/vi";
+
+export const en = {
+  ...PANZOOM_VI,
+  CLOSE: "Đóng",
+  NEXT: "Tiếp",
+  PREV: "Trước",
+  MODAL: "Nhấn phím ESC để đóng",
+  ERROR: "Đã xảy ra lỗi, vui lòng thử lại sau",
+  IMAGE_ERROR: "Không tìm thấy ảnh",
+  ELEMENT_NOT_FOUND: "Không tìm thấy HTML Element",
+  AJAX_NOT_FOUND: "Lỗi tải AJAX : Không tìm thấy",
+  AJAX_FORBIDDEN: "Lỗi tải AJAX : Hạn chế",
+  IFRAME_ERROR: "Lỗi tải trang",
+  TOGGLE_ZOOM: "Đổi mức thu phóng",
+  TOGGLE_THUMBS: "Chuyển đổi hình thu nhỏ",
+  TOGGLE_SLIDESHOW: "Chuyển đổi trình chiếu",
+  TOGGLE_FULLSCREEN: "Bật chế độ toàn màn hìnhe",
+  DOWNLOAD: "Tải xuống",
+};
